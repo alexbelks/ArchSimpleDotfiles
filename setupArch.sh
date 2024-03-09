c87c2ecbf513ca17c871417902261652df95bd2b
alexbelks <93944858+alexbelks@users.noreply.github.com>
2024-03-09 14:55:35 +0300

    Update setupArch.sh

--- setupArch.sh
+++ setupArch.sh
@@ -45,7 +45,7 @@ echo "root:$ROOT_PASSWORD" | chpasswd
 # Имя хоста
 echo "$HOSTNAME" > /etc/hostname
 
-pacman -Syu --noconfirm --needed nvidia grub efibootmgr sudo
+pacman -Syu --noconfirm --needed nvidia nvidia-utils nvidia-settings grub efibootmgr sudo
 
 # Настройка sudo
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
