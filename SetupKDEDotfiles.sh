a43908bfb4cd9a49dadc08b4e9e851930b5fb5af
alexbelks <93944858+alexbelks@users.noreply.github.com>
2024-03-09 15:03:42 +0300

    Update SetupKDEDotfiles.sh

--- SetupKDEDotfiles.sh
+++ SetupKDEDotfiles.sh
@@ -1,86 +1,24 @@
-#!/bin/bash
-
-# Остановка при ошибке
-set -e
-
-# Ввод имени пользователя
-echo "Введите имя нового пользователя:"
-read new_user
-
-# Ввод пароля пользователя
-echo "Введите пароль для нового пользователя:"
-read -s user_pass
-echo
-
-# Ввод пароля root
-echo "Введите пароль для пользователя root:"
-read -s root_pass
-echo
-
-# Обновление системного часа
-timedatectl set-ntp true
-
-# Установка основной системы
-pacstrap /mnt base linux linux-firmware nano intel-ucode amd-ucode networkmanager
-
-# Настройка fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Вход в chroot и настройка системы
-arch-chroot /mnt /bin/bash <<EOF
-
-# Настройка часового пояса
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
-
-# Локализация
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=ru" > /etc/vconsole.conf
-echo "FONT=cyr-sun16" >> /etc/vconsole.conf
-
-# Настройка сети
-echo "myarch" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 myarch.localdomain myarch" >> /etc/hosts
-
-# Создание инициализации initramfs
-mkinitcpio -P
-
-# Установка загрузчика
-pacman -S grub efibootmgr --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Установка пароля root
-echo "root:$root_pass" | chpasswd
 
 # Установка KDE Plasma и системных инструментов
-pacman -S xorg plasma plasma-wayland-session sddm konsole dolphin kate plasma-nm plasma-pa ark gwenview spectacle sudo --noconfirm
-systemctl enable sddm
-systemctl enable NetworkManager
-
-# Установка драйверов NVIDIA
-pacman -S nvidia nvidia-utils nvidia-settings --noconfirm
+sudo pacman -Syu --noconfirm --needed  xorg xorg-xinit xorg-server base-devel plasma plasma-wayland-session sddm konsole dolphin kate plasma-nm plasma-pa ark gwenview spectacle neovim nano --noconfirm
+sudo systemctl enable sddm
 
 # Установка Pipewire и настройка звука
-pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol --noconfirm
-systemctl enable pipewire pipewire-pulse
+sudo pacman -Syu --noconfirm --needed pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol 
+sudo systemctl enable pipewire pipewire-pulse
 
-# Создание нового пользователя
-useradd -m -G wheel,audio,video,network,storage -s /bin/bash $new_user
-echo "$new_user:$user_pass" | chpasswd
+sudo usermod -aG wheel,audio,video $USER
 
-# Настройка sudo для группы wheel
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-# Настройка переключения раскладок
-localectl set-x11-keymap ru,us pc104 , grp:ctrl_shift_toggle
-
-EOF
+if ! command -v yay &> /dev/null
+then
+    echo "yay не найден, начинаем установку..."
+    git clone https://aur.archlinux.org/yay.git ~/yay
+    cd ~/yay && makepkg -si --noconfirm --needed
+    cd ~
+    rm -rf ~/yay
+else
+    echo "yay уже установлен."
+fi
 
 # Сообщение о завершении установки и перезагрузка
 echo "Установка завершена. Система будет автоматически перезагружена через 5 секунд..."
