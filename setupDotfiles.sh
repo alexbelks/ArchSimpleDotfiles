50940baea81bc99ac746f6a0b39cd6ffaeb6ec0f
alexbelks <93944858+alexbelks@users.noreply.github.com>
2024-03-16 00:42:50 +0300

    Update setupDotfiles.sh

--- setupDotfiles.sh
+++ setupDotfiles.sh
@@ -35,7 +35,7 @@ echo good
 git --git-dir="$repo_dir" --work-tree="$work_tree" checkout -f
 
 # Установка основных пакетов
-sudo pacman -Syu --noconfirm --needed networkmanager neovim pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server base-devel xfce4 xfce4-goodies i3 xclip zsh feh fzf python-pip kitty pipx; 
+sudo pacman -Syu --noconfirm --needed networkmanager neovim pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server base-devel xfce4 xfce4-goodies i3 xclip zsh feh fzf python-pip kitty python-pipx; 
 sudo systemctl enable NetworkManager;
 
 
