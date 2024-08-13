868cb96a1160948348c3d807eabe69dea44d8c1e
alexbelks <93944858+alexbelks@users.noreply.github.com>
2024-08-13 14:05:27 +0300

    Update setupDotfiles.sh

--- setupDotfiles.sh
+++ setupDotfiles.sh
@@ -172,6 +172,6 @@ fi
 if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 fi
-pipx install thefuck
+
 source ~/.zshrc
 
